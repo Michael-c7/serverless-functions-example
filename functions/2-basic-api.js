7cabda60743a366a22064e7405af3347bc3fc7ff
Michael Carr <mcarr2468@gmail.com>
2022-05-12 14:58:21 -0500

    added a space

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context, cb) => {
     
     return {
         headers:{
-           'Access-Control-Allow-Origin':'*'
+           'Access-Control-Allow-Origin': '*'
         },
         statusCode:200,
         // body must be a string, can stringify not strings to meet this requirement
