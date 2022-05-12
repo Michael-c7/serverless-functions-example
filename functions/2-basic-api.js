5e79bef88022e988621bfa0c4eb29007325978f1
Michael Carr <mcarr2468@gmail.com>
2022-05-12 15:00:55 -0500

    CORS fix 4

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,16 +1,14 @@
 // domain/.netlify/functions/2-basic-api
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 
-const items = require("../assets/data")
+const items = require('../src/assets/data')
 
 exports.handler = async (event, context, cb) => {
-    
-    return {
-        headers:{
-           'Access-Control-Allow-Origin': '*'
-        },
-        statusCode:200,
-        // body must be a string, can stringify not strings to meet this requirement
-        body:JSON.stringify(items),
-    }
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: JSON.stringify(items),
+  }
 }
