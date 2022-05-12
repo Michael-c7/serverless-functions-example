a9ea02e6c777d86e6f65796aca20ece6942aad53
Michael Carr <mcarr2468@gmail.com>
2022-05-12 14:52:16 -0500

    fixed CORS

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -6,6 +6,9 @@ const items = require("../assets/data")
 exports.handler = async (event, context, cb) => {
     
     return {
+        headers:{
+            "Access-Control-Allow-Origin":"*"
+        },
         statusCode:200,
         // body must be a string, can stringify not strings to meet this requirement
         body:JSON.stringify(items),
